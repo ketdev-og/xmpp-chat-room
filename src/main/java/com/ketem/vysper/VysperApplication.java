02f6fc899ca6d9f8749b92a2fe663775bdfe4982
ketdev-og <clawrence@oasismgt.net>
2024-06-17 10:56:45 +0100

    xmpp serve connection - broken

--- src/main/java/com/ketem/vysper/VysperApplication.java
+++ src/main/java/com/ketem/vysper/VysperApplication.java
@@ -1,29 +1,50 @@
 package com.ketem.vysper;
 
-import org.apache.vysper.mina.S2SEndpoint;
+
+import org.apache.vysper.mina.TCPEndpoint;
+import org.apache.vysper.storage.OpenStorageProviderRegistry;
+import org.apache.vysper.storage.StorageProvider;
+import org.apache.vysper.storage.StorageProviderRegistry;
+import org.apache.vysper.storage.inmemory.MemoryStorageProviderRegistry;
+import org.apache.vysper.storage.jcr.JcrStorageProviderRegistry;
+import org.apache.vysper.xmpp.addressing.Entity;
+import org.apache.vysper.xmpp.authorization.UserAuthorization;
+import org.apache.vysper.xmpp.modules.roster.persistence.MemoryRosterManager;
 import org.apache.vysper.xmpp.server.XMPPServer;
-import org.apache.vysper.xmpp.server.s2s.XMPPServerConnector;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.directory.InitialDirContext;
 import java.net.http.WebSocket;
+import java.util.Hashtable;
 
 @SpringBootApplication
 public class VysperApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(VysperApplication.class, args);
+    public static void main(String[] args) throws Exception {
+        OpenStorageProviderRegistry providerRegistry = new OpenStorageProviderRegistry();
+
+        // add your custom authentication provider
+        providerRegistry.add(new MyLdapAuthentication());
+
+        // a roster manager is also required
+        providerRegistry.add(new MemoryRosterManager());
+
+        XMPPServer server = new XMPPServer("acme.com");
+
+        // other initialization
+        // ...
+
+        server.setStorageProviderRegistry(providerRegistry);
+
+        server.start();
+SpringApplication.run(VysperApplication.class, args);
+
     }
 
-    public void testIt() throws Exception {
-        XMPPServer xmppServer =  new XMPPServer("vysper.org");
-        xmppServer.addEndpoint(new S2SEndpoint());
-        xmppServer.start();
-        xmppServer.getServerRuntimeContext().getServerFeatures().setRelayingToFederationServers(true);
-
-
-
-
-    }
 
 }
+
